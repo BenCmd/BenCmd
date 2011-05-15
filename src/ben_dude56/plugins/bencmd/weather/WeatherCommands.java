b50f5d0cf83e484033961b2a872fdf5496b8a535
Ben Thomas <bendude56@gmail.com>
2011-05-15 08:14:06 -0600

    Added multi-world support to /storm

--- src/ben_dude56/plugins/bencmd/weather/WeatherCommands.java
+++ src/ben_dude56/plugins/bencmd/weather/WeatherCommands.java
@@ -2,6 +2,7 @@ package ben_dude56.plugins.bencmd.weather;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -41,23 +42,40 @@ public class WeatherCommands implements Commands {
 	}
 	
 	public void Storm(String[] args, User user) {
-		if(args.length != 1) {
-			user.sendMessage(ChatColor.YELLOW + "Proper use is /storm {off|rain|thunder}");
+		if(args.length != 1 && args.length != 2) {
+			user.sendMessage(ChatColor.YELLOW + "Proper use is /storm {off|rain|thunder} [world]");
 			return;
 		}
+		World world;
+		if(args.length == 2) {
+			if((world = plugin.getServer().getWorld(args[1])) == null) {
+				user.sendMessage(ChatColor.RED + "World " + args[1] + " wasn't found!");
+				return;
+			}
+		} else {
+			if(user.isServer()) {
+				world = plugin.getServer().getWorlds().get(0);
+			} else {
+				world = user.getHandle().getWorld();
+			}
+		}
 		if(args[0].equalsIgnoreCase("off")) {
-			user.getHandle().getWorld().setStorm(false);
-			user.getHandle().getWorld().setThundering(false);
+			world.setStorm(false);
+			world.setThundering(false);
 		} else if (args[0].equalsIgnoreCase("rain")) {
-			user.getHandle().getWorld().setStorm(true);
-			user.getHandle().getWorld().setThundering(false);
+			world.setStorm(true);
+			world.setThundering(false);
 		} else if (args[0].equalsIgnoreCase("thunder")) {
-			user.getHandle().getWorld().setStorm(true);
-			user.getHandle().getWorld().setThundering(true);
+			world.setStorm(true);
+			world.setThundering(true);
 		}
 	}
 	
 	public void Strike(String[] args, User user) {
+		if(user.isServer()) {
+			user.sendMessage(ChatColor.RED + "The server cannot do that!");
+			return;
+		}
 		Block targetBlock = user.getHandle().getTargetBlock(null, 100);
 		Location loc = targetBlock.getLocation();
 		user.getHandle().getWorld().strikeLightning(loc);
