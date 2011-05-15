f9d336ecabd16c37d51f0fd89ddf7a40030247aa
Ben Thomas <bendude56@gmail.com>
2011-05-14 20:16:42 -0600

    Fixed a glitch in the fly detection where a player standing at the edge of the block was detected as being in mid-air.

--- src/ben_dude56/plugins/bencmd/nofly/FlyTimer.java
+++ src/ben_dude56/plugins/bencmd/nofly/FlyTimer.java
@@ -13,13 +13,54 @@ public class FlyTimer extends TimerTask {
 	public FlyTimer(FlyDetect detect) {
 		flyDetect = detect;
 	}
+	
+	public boolean onBlock(Player player) {
+		Location loc = player.getLocation();
+		loc.setY(loc.getY() - 1);
+		loc.setX(loc.getX() - 1);
+		loc.setZ(loc.getZ() - 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setZ(loc.getZ() + 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setZ(loc.getZ() + 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setX(loc.getX() + 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setZ(loc.getZ() - 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setZ(loc.getZ() - 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setX(loc.getX() + 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setZ(loc.getZ() + 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		loc.setZ(loc.getZ() + 1);
+		if(loc.getBlock().getType() != Material.AIR) {
+			return true;
+		}
+		return false;
+	}
 
 	@Override
 	public void run() {
 		for(Player player : flyDetect.plugin.getServer().getOnlinePlayers()) {
-			Location playerLocation = player.getLocation();
-			playerLocation.setY(playerLocation.getY() - 1);
-			if(playerLocation.getBlock().getType() == Material.AIR) {
+			if(!onBlock(player)) {
 				flyDetect.detect(player);
 			} else {
 				flyDetect.undetect(player);
