fadb77594b9b11d56bd714817200cc9a2b914bb9
Deaboy <danieleandrus@gmail.com>
2011-07-29 01:53:34 -0600

    Wooden shovels no longer cause/receive damage when in the hands of landlords, except when used against creatures and players.

--- src/ben_dude56/plugins/bencmd/lots/LotBlockListener.java
+++ src/ben_dude56/plugins/bencmd/lots/LotBlockListener.java
@@ -1,12 +1,14 @@
 package ben_dude56.plugins.bencmd.lots;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 import ben_dude56.plugins.bencmd.BenCmd;
+import ben_dude56.plugins.bencmd.User;
 
 public class LotBlockListener extends BlockListener {
 
@@ -19,6 +21,11 @@ public class LotBlockListener extends BlockListener {
 	}
 
 	public void onBlockBreak(BlockBreakEvent event) {
+		
+		if (player.getItemInHand().getType() == Material.WOOD_SPADE && User.getUser(plugin, player).hasPerm("isLandlord")) {
+			event.setCancelled(true);
+		}
+		
 		Player player = event.getPlayer();
 		if (!plugin.lots.canBuildHere(player, event.getBlock().getLocation())) {
 			event.setCancelled(true);
